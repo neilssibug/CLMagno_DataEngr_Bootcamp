c0c88b6f7c2773928bed0b10eea65b544b7da1c5
Neil Sibug <neilssibug@gmail.com>
2026-05-23 19:42:05 +0800

    initial commit

---
+++ hello_data.py
@@ -0,0 +1,7 @@
+#Python
+import sys
+print("--------------------------------------")
+print("System Check for CL Magno Bootcamp:")
+print(f"Python Version: {sys.version}")
+print("I am ready to become a Data Engineer!")
+print("--------------------------------------")
