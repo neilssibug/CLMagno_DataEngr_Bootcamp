e203985f4287221e9db8ad92f74de8b0837fb7ed
Neil Sibug <neilssibug@gmail.com>
2026-05-29 23:51:50 +0800

    added all the activities

--- May_23_2026/activity.py
+++ May_23_2026/activity.py
@@ -1,5 +1,6 @@
 #print("Hello World!")
 import time
+from datetime import datetime
 
 choice = ""
 
@@ -12,6 +13,7 @@ while choice != "X" :
     print("[C] Math operations")
     print("[D] Logical operations")
     print("[E] Validation")
+    print("[F] Looping")
     print("[X] Exit")
 
 
@@ -25,12 +27,12 @@ while choice != "X" :
             print("Variables:\n")
             is_string = "Welcome to Variables"
             is_integer = 10
-            is_float = 1.5
+            is_float = 1.5596
             is_boolean = True
 
             print(f"This is a {type(is_string)} : {is_string}")
             print(f"This is a {type(is_integer)} : {is_integer}")
-            print(f"This is a {type(is_float)} : {is_float}")
+            print(f"This is a {type(is_float)} : {is_float:,.2f}")
             print(f"This is a {type(is_boolean)} : {is_boolean}")
 
             """
@@ -54,6 +56,13 @@ while choice != "X" :
         while in_choice != "y":
             print("Type cast & Transform:\n")
 
+            first = input("Input your First Name: ").title()
+            last = input("Input your Last Name: ").title()
+            age = int(input("What is your Age: "))
+
+            full_name = f"{first.strip()} {last.strip()}"
+            print(f"{full_name} you're {age} y/o now!")
+
             in_choice = input("\nBack: (y/n): ").lower()
 
     elif choice == "C":
@@ -61,6 +70,11 @@ while choice != "X" :
         
         while in_choice != "y":
             print("Math operations:\n")
+
+            birth_year = int(input("What is your birth year: "))
+            age_now = datetime.now().year - birth_year
+
+            print(f"You are {age_now} y/o")
 
             in_choice = input("\nBack: (y/n): ").lower()
 
@@ -70,6 +84,17 @@ while choice != "X" :
         while in_choice != "y":
             print("Logical operations:\n")
 
+            mom_year = int(input("What is Mom's birth year: "))
+            you_year = int(input("What is your birth year: "))
+
+            mom_age = datetime.now().year - mom_year
+            your_age = datetime.now().year - you_year
+
+            if mom_age >= your_age:
+                print(f"Your definitely younger than your Mom!")            
+            elif mom_age <= your_age:
+                print(f"What!!! you look older than your Mom")
+
             in_choice = input("\nBack: (y/n): ").lower()
 
     elif choice == "E":
@@ -78,6 +103,25 @@ while choice != "X" :
         while in_choice != "y":
             print("Validation:\n")
 
+            your_age = int(input("What is your age: "))
+
+            if your_age >= 18:
+                print("Valid")
+            else:
+                print("Invalid!!!")
+
+            in_choice = input("\nBack: (y/n): ").lower()
+
+    elif choice == "F":
+        in_choice = "n"
+        
+        while in_choice != "y":
+            print("Looping:\n")
+
+            for i in range(10):
+                if i % 2:
+                    print(i)
+        
             in_choice = input("\nBack: (y/n): ").lower()
 
 print("Thank you!!!")
